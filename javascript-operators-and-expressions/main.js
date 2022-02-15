7419929bb4c772f636006e58c990d6ef4ab055e7
codewithbingle <codewithbingle@gmail.com>
2022-02-14 21:22:17 -0800

    Completed javascript-operators-and-expressions Exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,52 @@
+// Multiply
+var width = 12;
+var height = 4;
+
+var area = width * height;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+// Subtract
+var bill = 10;
+var payment = 18;
+
+var change = payment - bill;
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+// Calculate the Average
+var quizzes = 97;
+var midterm = 98;
+var final = 95;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+// String Concatenation
+var firstName = 'Bingle';
+var lastName = 'Padilla';
+
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+// Less-Than Comparison
+var ph = 2;
+
+var isAcidic = ph < 7;
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+// Equality Comparison
+var headCount = 40;
+
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+// Addition Assignment
+var motto = fullName;
+
+console.log('value of motto: ', motto + ' ' + 'is the GOAT');
+console.log('typeof motto: ', typeof motto);
