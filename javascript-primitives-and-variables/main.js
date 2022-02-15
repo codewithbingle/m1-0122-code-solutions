387356e1a307d0cacf8782fc1f5f1f79b06d6b51
codewithbingle <codewithbingle@gmail.com>
2022-02-14 19:34:23 -0800

    Completed javascript-primitives-and-variables Exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Bingle Padilla';
+var isCool = true;
+var totalPets = 1;
+var unknown = null;
+var never;
+
+console.log('value of fullName: ' + fullName);
+console.log('value of isCool: ' + isCool);
+console.log('value of totalPets: ' + totalPets);
+console.log('typeof fullName: ' + typeof 'Bingle Padilla');
+console.log('typeof isCool: ' + typeof true);
+console.log('typeof totalPets: ' + typeof 1);
+console.log('value of unknown: ' + unknown);
+console.log('typeof unknown: ' + typeof null);
+console.log('value of never: ' + never);
+console.log('typeof never: ' + typeof never);
