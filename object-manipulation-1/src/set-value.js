6c41fbfe44ffbd642288317e12a0fedf752b0aba
codewithbingle <codewithbingle@gmail.com>
2022-03-03 19:37:52 -0800

    Completed object-manipultaion-1 Exercise

---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,13 @@
+/* exported setValue */
+
+// 2 setValue(object, key, value)
+
+// Before Pseudo Code
+// define a function named setValue, which has three parameters 'object', 'key', 'value'
+// return nothing
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
+
+// After Pseudo Code
