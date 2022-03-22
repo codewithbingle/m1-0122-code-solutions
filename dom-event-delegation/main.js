094c977bc3013f105aa50fa35da87eb527a79ab9
codewithbingle <codewithbingle@gmail.com>
2022-03-21 20:58:33 -0700

    Added Alternative Solution

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -7,9 +7,27 @@ function handleClick(event) {
   console.log('event.target: ', event.target);
   console.log('event.target.tagName: ', event.target.tagName);
   if (event.target && event.target.tagName === 'BUTTON') {
-    // if (event.target.tagName !=== 'BUTTON')
+    // if (event.target.tagName !=== 'BUTTON') {return;}
     var $liElement = event.target.closest('.task-list-item');
     console.log('closet .task-list-item: ', $liElement);
     $liElement.remove();
   }
 }
+
+// alternative solution
+
+/* var $taskList = document.querySelector('.task-list);
+
+$taksList.addEventListener('click' , function(event) {
+  console.log('event.target: ', event.target);
+  console.log('event.target: ', event.target.tagName);
+
+  if (event.target.tagName !=== 'BUTTON') {
+    return;
+  }
+
+  var $taskListItem = event.target.closest('.task-list-item');
+  console.log('closest .task-list-item', $taskListItem);
+  $taskListItem.remove();
+
+}) */
