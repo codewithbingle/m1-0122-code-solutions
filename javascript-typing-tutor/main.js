2e12c8435694c73afd10afe559499de64020bb8a
codewithbingle <codewithbingle@gmail.com>
2022-03-24 22:10:41 -0700

    Completed javascript-typing-tutor Challenge

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,2 +1,29 @@
-// eslint-disable-next-line no-unused-vars
+var typingCounter = 0;
+
 var $spans = document.querySelectorAll('span');
+
+var $messageComplete = document.querySelector('.row-completion-reset');
+
+var $buttonReset = document.querySelector('.reset-button');
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === $spans[typingCounter].textContent) {
+    $spans[typingCounter].classList.remove(
+      'border-bottom',
+      'red-error-text');
+    $spans[typingCounter].classList.add(
+      'green-correct-text');
+    typingCounter++;
+    if (typingCounter === $spans.length) {
+      $messageComplete.style.display = 'flex';
+      return;
+    }
+    $spans[typingCounter].classList.add('border-bottom');
+  } else {
+    $spans[typingCounter].classList.add('red-error-text');
+  }
+});
+
+$buttonReset.addEventListener('click', function (event) {
+  location.reload();
+});
